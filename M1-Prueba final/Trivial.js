26e9f335283eceb32c73a79ac95c7f8b41ea535e
Julián Rincón Morales <julianrinconm@gmail.com>
2020-10-12 13:25:11 +0200

    Renombro al jeugo correcto, inicio creacion clase pregunta

--- M1-Prueba final/Trivial.js
+++ M1-Prueba final/Trivial.js
@@ -1,3 +1,9 @@
 /* 2 clases, pregunta, y partida. 
 Creditos a Lionel de las preguntas de saber y ganar
 */
+class Pregunta {
+    constructor (){
+        let pregunta;
+        let respuesta;
+    }
+}
