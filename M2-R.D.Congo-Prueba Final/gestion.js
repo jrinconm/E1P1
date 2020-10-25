da1414d0291420fc6e301870b939a900605ed512
Julián Rincón Morales <julianrinconm@gmail.com>
2020-10-25 11:43:09 +0100

    Creacion de funciones para mostrar objetos. Pendiente de resolver el mostrar el objeto de objeto.

--- M2-R.D.Congo-Prueba Final/gestion.js
+++ M2-R.D.Congo-Prueba Final/gestion.js
@@ -2,6 +2,47 @@
 var hospital=new Hospital("El consuelo","Valencia","Pepe");
 var personal1=new Personal("Pepe","medico",hospital);
 var paciente1=new Paciente("Jose","Infarto",personal1);
-console.log(hospital);
-console.log(personal1);
-console.log(paciente1);
+// Recorre un objeto para crear una fila
+// Si valor es true, quiero los valores del objeto
+function creaFila(objeto,tipoCelda,valor){
+    let fila = document.createElement("tr");
+    for(const propiedad in objeto){
+        let celda = document.createElement(tipoCelda);
+        if(valor){
+            let textoCelda = document.createTextNode(objeto[propiedad]);
+            celda.appendChild(textoCelda);
+            celda.style.textTransform = "capitalize";
+            fila.appendChild(celda);
+        } else {
+            let textoCelda = document.createTextNode(propiedad);
+            celda.appendChild(textoCelda);
+            celda.style.textTransform = "capitalize";
+            fila.appendChild(celda);
+        }
+    }
+    return fila
+}
+function creaTabla(objeto){
+    // Creo una tabla
+    let tabla=document.createElement("table");
+    let cuerpoTabla=document.createElement("tbody");
+    // La primera fila es la cabecera
+    let fila = creaFila(objeto,"th");
+    cuerpoTabla.appendChild(fila);
+    // La seunda fila son los valores. Indico true para que recoja los valores
+    fila = creaFila(objeto,"td",true);
+    cuerpoTabla.appendChild(fila);
+    // Añado el cuerpo a la tabla
+    tabla.appendChild(cuerpoTabla);
+    // Devuelvo la tabla
+    return tabla;
+}
+function muestraDatos(){
+    let tabla=creaTabla(hospital);
+    document.getElementById("visualizacion").appendChild(tabla);
+    tabla=creaTabla(personal1);
+    document.getElementById("visualizacion").appendChild(tabla);
+    tabla=creaTabla(paciente1);
+    document.getElementById("visualizacion").appendChild(tabla);
+}
+window.onload=muestraDatos;
