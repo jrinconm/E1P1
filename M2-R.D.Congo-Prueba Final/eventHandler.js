1d6a40881a2f7d8c8f6d8f2e4dc0f5f2cdf5c0a2
Rincon Morales - Julian <jrincon@consum.red>
2020-10-26 07:45:52 +0100

    Separo la gestion de eventos a fichero aparte

---
+++ M2-R.D.Congo-Prueba Final/eventHandler.js
@@ -0,0 +1,48 @@
+// Funcion para ejecutar cuando se hace click
+// Ejecuta una funcion basada en la lista de funciones de window
+function click(ev){
+    // Es sucio, pero me permite llamar una funcion por el nombre de la lista de funciones de la ventana
+    window[ev.target.name]();
+}
+function pacienteAlta(){
+    // Borro y creo el div de paciente
+    creaBorraDiv("paciente");
+    let propiedades=["Nombre","Enfermedad","Hospital","Personal"];
+    creaFormulario(propiedades,"visualizacion"+"paciente");    
+    //hospitales[0].personal[0].addPaciente(new Paciente("Jose","Infarto"));
+}
+function personalAlta(){
+    // Borro y creo el div de personal
+    creaBorraDiv("personal");
+    let propiedades=["Nombre","Especialidad","Hospital"];
+    creaFormulario(propiedades,"visualizacion"+"personal");    
+}
+function hospitalAlta(){
+    creaBorraDiv("hospital");
+    let propiedades=["Nombre","Localidad","Responsable"];
+    creaFormulario(propiedades,"visualizacion"+"hospital");   
+    //hospitales.push(new Hospital("El consuelo","Valencia","Pepe"));
+}
+function hospitalMostrar(){
+    // Borro y creo el div de personal
+    creaBorraDiv("hospital");
+    creaTablaObjeto(hospitales,"visualizacion"+"hospital");    
+}
+function personalMostrar(){
+    // Borro y creo el div de personal
+    creaBorraDiv("personal");
+    // Hago un bucle para revisar el personal de cada hospital
+    for (const hospital in hospitales){
+        let elemento=document.getElementById("visualizacion"+"personal");
+        let texto="El hospital: " + hospitales[hospital].nombre + " tiene el siguiente personal:";
+        let cabecera = creaCabecera(texto,"h2");
+        elemento.append(cabecera);
+        let tabla=creaTablaObjeto(hospitales[hospital].personal)
+        elemento.append(tabla); 
+    } 
+}
+function pacienteMostrar(){
+    // Borro y creo el div de personal
+    creaBorraDiv("paciente");
+    creaTablaObjeto(hospitales,"visualizacion"+"paciente");    
+}
--- M2-R.D.Congo-Prueba Final/gestion.js
+++ M2-R.D.Congo-Prueba Final/gestion.js
@@ -138,54 +138,7 @@ function creaBorraDiv(elemento){
     // Borro el contenido del div 
     document.getElementById("visualizacion"+elemento).innerHTML="";
 }
-// Funcion para ejecutar cuando se hace click
-// Ejecuta una funcion basada en la lista de funciones de window
-function click(ev){
-    // Es sucio, pero me permite llamar una funcion por el nombre de la lista de funciones de la ventana
-    window[ev.target.name]();
-}
-function pacienteAlta(){
-    // Borro y creo el div de paciente
-    creaBorraDiv("paciente");
-    let propiedades=["Nombre","Enfermedad","Hospital","Personal"];
-    creaFormulario(propiedades,"visualizacion"+"paciente");    
-    //hospitales[0].personal[0].addPaciente(new Paciente("Jose","Infarto"));
-}
-function personalAlta(){
-    // Borro y creo el div de personal
-    creaBorraDiv("personal");
-    let propiedades=["Nombre","Especialidad","Hospital"];
-    creaFormulario(propiedades,"visualizacion"+"personal");    
-}
-function hospitalAlta(){
-    creaBorraDiv("hospital");
-    let propiedades=["Nombre","Localidad","Responsable"];
-    creaFormulario(propiedades,"visualizacion"+"hospital");   
-    //hospitales.push(new Hospital("El consuelo","Valencia","Pepe"));
-}
-function hospitalMostrar(){
-    // Borro y creo el div de personal
-    creaBorraDiv("hospital");
-    creaTablaObjeto(hospitales,"visualizacion"+"hospital");    
-}
-function personalMostrar(){
-    // Borro y creo el div de personal
-    creaBorraDiv("personal");
-    // Hago un bucle para revisar el personal de cada hospital
-    for (const hospital in hospitales){
-        let elemento=document.getElementById("visualizacion"+"personal");
-        let texto="El hospital: " + hospitales[hospital].nombre + " tiene el siguiente personal:";
-        let cabecera = creaCabecera(texto,"h2");
-        elemento.append(cabecera);
-        let tabla=creaTablaObjeto(hospitales[hospital].personal)
-        elemento.append(tabla); 
-    } 
-}
-function pacienteMostrar(){
-    // Borro y creo el div de personal
-    creaBorraDiv("paciente");
-    creaTablaObjeto(hospitales,"visualizacion"+"paciente");    
-}
+
 /* Creo un boton combinando:
 Tipo(alta, baja, modificacion y mostrar) y Elemento(hospita, personal, paciente)
 El nombre del boton combina elemento+tipo y llama a una funcion llamada click
