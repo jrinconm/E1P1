02aa17c51066b0f241ecb7f426f9b648c6007d0e
Julián Rincón Morales <julianrinconm@gmail.com>
2020-10-31 20:04:30 +0100

    Al marcar club activa y desactiva campos Fin de creacion funciones check vacias

--- M3-Norteamerica/u3e2/validacion.js
+++ M3-Norteamerica/u3e2/validacion.js
@@ -1,53 +1,43 @@
 window.onload=iniciar;
 function iniciar(){
     document.getElementById("enviar").addEventListener('click',validar,false);
+    document.getElementById("club").addEventListener('click',pincharClub,false);
 }
 function validar(){
     validaIdentificador();
     validaNombre();
 }
+function pincharClub(){
+    if(document.getElementById("club").checked){
+        document.getElementById("socio").disabled=false;
+        document.getElementById("infantil").disabled=false;
+        document.getElementById("juvenil").disabled=false;
+        document.getElementById("senior").disabled=false;
+    } else {
+        document.getElementById("socio").disabled=true;   
+        document.getElementById("infantil").disabled=true;
+        document.getElementById("juvenil").disabled=true;
+        document.getElementById("senior").disabled=true;     
+    }
+}
 function validaIdentificador(){
     let elemento = document.getElementById("identificador");
 }
 function validaNombre(){
-    let elemento = document.getElementById("identificador");
+    let elemento = document.getElementById("nombre");
 }
-/**     
-    <input type="text" name="nombre" id="nombre"
-    maxlength="50" required/>
-
-    <input type="text" name="fechaNacimiento" id="fechaNacimiento"
-    maxlength="10" pattern="(\d){2}/(\d){2}/(\d){4}"required/>
-
-    <input type="text" name="correo" id="correo"
-    maxlength="20" 
-    pattern="[A-Za-z0-9\._\-]{1,20}@[A-Za-z]{1,20}\.[A-Za-z]{2,3}" 
-
-    <input type="text" name="telefono" id="telefono"
-    maxlength="20" 
-    pattern="\+[0-9]{2,3}\-[0-9]{9}" 
-    />
-
-    <select name="edad" id="edad">
-        <option value="niño">Niño</option>
-        <option value="adulto">Adulto</option>
-        <option value="jubilado">Jubilado</option>
-    </select>
-
-    <input type="checkbox" name="club" id="club">
-
-    <input type="text" name="socio" id="socio"
-    maxlength="5" 
-    pattern="[1-9]{1}[0-9]{4}" 
-    disabled />
-</td>                
-</tr> 
-<tr>
-<td>Categoría</td>
-<td >
-    <input type="radio" name="categoria" id="categoria" value="infantil"  disabled checked/>  Infantil
-    <input type="radio" name="categoria" id="categoria" value="juvenil" disabled /> Juvenil
-    <input type="radio" name="categoria" id="categoria" value="senior" disabled /> Senior           
-</td>                
-</tr> 
-*/
+function validaFechaNacimiento(){
+    let elemento = document.getElementById("fechaNacimiento");
+}
+function validaCorreo(){
+    let elemento = document.getElementById("correo");
+}
+function validaTelefono(){
+    let elemento = document.getElementById("telefono");
+}
+function validaEdad(){
+    let elemento = document.getElementById("edad");
+}
+function validaSocio(){
+    let elemento = document.getElementById("socio");
+}
