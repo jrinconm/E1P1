9e1fdb1aa6c377428ba4780fca8d912dfc20d432
Julián Rincón Morales <julianrinconm@gmail.com>
2020-10-31 13:50:41 +0100

    No funcionan las clases. Pendiente comprobacion.

--- M3-Norteamerica/u3e1/Partida.js
+++ M3-Norteamerica/u3e1/Partida.js
@@ -1,23 +1,27 @@
 "use strict";
 class Partida {
     // Inicializo los valores
-    constructor (){
+    constructor (parejas){
         // Contador de aciertos
         this.aciertos=0;
         this.fallos=0;  
+        this.parejas=parejas;
         this.cartas=[];      
     }
-    // Metodo que comprueba si has ganado o perdido
+    // Metodo que comprueba si has acabado de voltear todas la cartas
     finPartida(){
         if(this.aciertos === this.cartas.length/2) return true;
         else return false;
     }
-    jugar(){
-        // Creo las 4 parejas de cartas
-        for(let pareja=0;pareja <= 4; pareja++){
+    generaCartas(){
+        // Creo las parejas de cartas
+        for(let pareja=0;pareja <= this.parejas; pareja++){
             // Tengo las cartas numeradas del 1 al 5 en Imagenes
             this.cartas.push(new Carta(pareja,"Imagenes/"+(pareja+1)+".png"));
+            this.cartas.push(new Carta(pareja,"Imagenes/"+(pareja+1)+".png"));
         }
-        this.resultados();
+    }
+    jugar(){
+
     }
 }
--- M3-Norteamerica/u3e1/Tablero.js
+++ M3-Norteamerica/u3e1/Tablero.js
@@ -1,5 +1,28 @@
 "use strict";
 function creaImg(carta){
     let imagen = document.createElement("img");
-    img.src=carta.imagen;
+    imagen.src=carta.imagen;
+}
+function jugar(partida){
+        // Genero las cartas necesarias
+        partida.generaCartas();
+        // Muestro las cartas
+        partida.muestraCartas();
+        //if(finPartida()){
+        //   console.log("Se acabo")
+        //}
+}
+let partida=new Partida(4);
+window.onload=jugar;
+function muestraCartas(){
+    console.log(this.cartas);
+    let div = document.createElement("div");
+    div.setAttribute("id", "Juego");
+    document.body.append(div);
+    for(const idCarta of this.cartas){
+        console.log(idCarta);
+        let imagen=creaImg(idCarta.imagen);
+        console.log(imagen);
+        div.append(imagen);
+    }
 }
