f85fc3ccc640c05f709d3dbb2cee68967ba8e7c2
Julián Rincón Morales <julianrinconm@gmail.com>
2020-10-31 23:58:32 +0100

    Finalizados regexp. Forzado que regexp empeice y acabe Pendiente pruebas

--- M3-Norteamerica/u3e3/comprobar.js
+++ M3-Norteamerica/u3e3/comprobar.js
@@ -4,18 +4,33 @@ function iniciar(){
     // Añado el listener al boton enviar
     document.getElementById("fechaBoton").addEventListener('click',validarFecha,false);
     document.getElementById("cocineroBoton").addEventListener('click',validarCocinero,false);
-    document.getElementById("destinatarioBoton").addEventListener('click',validarFecha,false);
-    document.getElementById("gramosBoton").addEventListener('click',validarFecha,false);
-    document.getElementById("composicionBoton").addEventListener('click',validarFecha,false);
+    document.getElementById("destinatarioBoton").addEventListener('click',validarDestinatario,false);
+    document.getElementById("gramosBoton").addEventListener('click',validarGramos,false);
+    document.getElementById("composicionBoton").addEventListener('click',validarComposicion,false);
 }
 function validarFecha(){
-    let patron=/(\d){2}\/(\d){2}\/(\d){4}\ (\d){2}:(\d){2}/;
+    let patron=/^(\d){2}\/(\d){2}\/(\d){4}\ (\d){2}:(\d){2}$/;
     valida("fecha",patron);
 }
 function validarCocinero(){
     // Por caracter entendi algo que no sea "word"
-    let patron=/[A-Z]{2}[^\w][\d]{4}/;
+    let patron=/^[A-Z]{2}[^\w][\d]{4}$/;
     valida("cocinero",patron);
+}
+function validarDestinatario(){
+    // Por caracter entendi algo que no sea "word"
+    let patron=/^[A-Z]{2,3}_[a-z]{1,}:[\d]{4}$/;
+    valida("destinatario",patron);
+}
+function validarGramos(){
+    // Entre el 1000 y el 4999 o 5000
+    let patron=/^([1-4]{1}[0-9]{3}|5000)$/;
+    valida("gramos",patron);
+}
+function validarComposicion(){
+    // Entre el 1000 y el 4999 o 5000
+    let patron=/^\d]{1,}g([A-Za-z]{1,2}[\d]{0,1}){2}$/;
+    valida("composicion",patron);
 }
 // Borra la X si es correcta
 function acierto(elemento){
