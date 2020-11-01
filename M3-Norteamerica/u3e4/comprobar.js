b27365c06635d4f60b86ddaf4a6543bffcbf2b27
Julián Rincón Morales <julianrinconm@gmail.com>
2020-11-01 09:23:42 +0100

    Creación estructura y funciones basicas

---
+++ M3-Norteamerica/u3e4/comprobar.js
@@ -0,0 +1,17 @@
+window.onload=iniciar;
+// Funcion principal al cargar el documento
+function iniciar(){
+    creaFormAutentificacion();
+}
+function creaFormAutentificacion(){
+
+}
+function creaFormFirma(){
+
+}
+function validar(){
+    
+}
+function alerta(){
+    alert("Ponte a trabajar, no seas Jessie Pinkman");
+}
